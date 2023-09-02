0717a4c66df785ffac48490340b79b3316f494bc
Pusikovsa <s.puzikov59@gmail.com>
2023-09-02 14:54:54 +0300

    added classes PlacesPage & AddedPlacePage

--- lib/pages/added_place_page.dart
+++ lib/pages/added_place_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AddedPlacePage extends StatelessWidget{
+  static const String rout = '/add_place';
+
   const AddedPlacePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Новое место'),
+      ),
+    );
   }
 
 }
