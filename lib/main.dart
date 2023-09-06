cb71bbbfa82f6adc64aff4bd18466e1b143f3785
Pusikovsa <s.puzikov59@gmail.com>
2023-09-06 12:31:59 +0300

    implemented storage MyPlaces

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_memorable_places/models/place.dart';
+import 'package:my_memorable_places/models/place_location.dart';
 import 'package:my_memorable_places/pages/added_place_page.dart';
 import 'package:my_memorable_places/pages/places_page.dart';
 import 'package:my_memorable_places/providers/my_places.dart';
@@ -7,6 +9,8 @@ import 'package:provider/provider.dart';
 
 void main() async{
   await Hive.initFlutter();
+  Hive.registerAdapter<Place>(PlaceAdapter());
+  Hive.registerAdapter<PlaceLocation>(PlaceLocationAdapter());
   
   runApp(const MyApp());
 }
--- lib/pages/places_page.dart
+++ lib/pages/places_page.dart
@@ -3,44 +3,56 @@ import 'package:my_memorable_places/pages/added_place_page.dart';
 import 'package:my_memorable_places/providers/my_places.dart';
 import 'package:provider/provider.dart';
 
+import '../models/place.dart';
+
 class PlacesPage extends StatelessWidget {
   const PlacesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Памятные места'),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, AddedPlacePage.rout);
-                },
-                icon: const Icon(Icons.add))
-          ],
-        ),
-        body: Consumer<MyPlaces>(
-          builder: (context, myPlaces, child) => myPlaces.items.isEmpty
-              ? child!
-              : ListView.builder(
-                  itemCount: myPlaces.items.length,
-                  itemBuilder: (context, index) => ListTile(
-                    title: Text(myPlaces.items[index].title),
-                    onTap: () {
-                      //TODO go to detail page
-                    },
-                    leading: CircleAvatar(
-                      backgroundImage: FileImage(myPlaces.items[index].image),
+      appBar: AppBar(
+        title: const Text('Памятные места'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AddedPlacePage.rout);
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: FutureBuilder(
+          future:
+              Provider.of<MyPlaces>(context, listen: false).fetchAndSetData(),
+          builder: (context, snapshot) {
+            return snapshot.connectionState == ConnectionState.waiting
+            ? const Center(
+              child: CircularProgressIndicator(),
+            )
+            : Consumer<MyPlaces>(
+              builder: (context, myPlaces, child) => myPlaces.items.isEmpty
+                  ? child!
+                  : ListView.builder(
+                      itemCount: myPlaces.items.length,
+                      itemBuilder: (context, index) => ListTile(
+                        title: Text(myPlaces.items[index].title),
+                        onTap: () {
+                          //TODO go to detail page
+                        },
+                        leading: CircleAvatar(
+                          backgroundImage:
+                              FileImage(myPlaces.items[index].image),
+                        ),
+                      ),
                     ),
-                  ),
+              child: const Center(
+                child: Text(
+                  'Не найдено ни одного места.\nСамое время добавить новое!',
+                  textAlign: TextAlign.center,
                 ),
-          child: const Center(
-            child: Text(
-              'Не найдено ни одного места.\nСамое время добавить новое!',
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
+              ),
+            );
+          }),
     );
   }
 }
